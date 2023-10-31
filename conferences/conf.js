5f58bbca6c58dae8732afa4d542c84ea2cdf5d79
Alexandre <cardaillac.alexandre@gmail.com>
2023-10-31 11:29:21 +1100

    Add conf list

--- conferences/conf.js
+++ conferences/conf.js
@@ -1,20 +1,146 @@
 conferences = [{
-        "title": "IEEE Best conf",
-        "abbrev": "BC 2023",
-        "hindex": 100,
-        "location": "Sydney, Australia",
-        "flag": "au",
-        "cfp": "22 December 2023",
-        "date": "May 13-17, 2024",
-        "link": "https://github.com/"
+        "hindex": 212,
+        "abbrev": "AAAI 2024",
+        "title": "AAAI Conference on Artificial Intelligence",
+        "location": "Philadelphia, USA",
+        "flag": "us",
+        "cfp": "8 August 2024 (TBA)",
+        "date": "February 20-27, 2025 (TBA)",
+        "link": "https://aaai.org/aaai-conference"
     },{
-        "title": "IEEE Super conf",
-        "abbrev": "SC 2023",
-        "hindex": 75,
-        "location": "Tokyo, Japan",
+        "hindex": 60,
+        "abbrev": "ACCV 2024",
+        "title": "Asian Conference on Computer Vision",
+        "location": "Hanoi, Vietnam",
+        "flag": "vi",
+        "cfp": "1 July 2024 (TBA)",
+        "date": "December 08-12, 2024",
+        "link": "https://accv2024.org/"
+    },{
+        "hindex": 77,
+        "abbrev": "BMVC 2024",
+        "title": "British Machine Vision Conference",
+        "location": "(TBA), UK",
+        "flag": "uk",
+        "cfp": "12 May 2024 (TBA)",
+        "date": "November 20-24, 2024 (TBA)",
+        "link": "https://www.bmva.org/bmvc"
+    },{
+        "hindex": 309,
+        "abbrev": "NeurIPS 2024",
+        "title": "Conference on Neural Information Processing Systems",
+        "location": "Vancouver, Canada",
+        "flag": "ca",
+        "cfp": "11 May 2024 (TBA)",
+        "date": "December 10-16, 2024 (TBA)",
+        "link": "https://nips.cc/Conferences/2024"
+    },{
+        "hindex": "/",
+        "abbrev": "Oceans 2024",
+        "title": "IEEE OCEANS 2024 Halifax",
+        "location": "Halifax, Canada",
+        "flag": "ca",
+        "cfp": "12 April 2024",
+        "date": "September 23-26, 2024",
+        "link": "https://halifax24.oceansconference.org"
+    },{
+        "hindex": 58,
+        "abbrev": "ICPR 2024",
+        "title": "International Conference on Pattern Recognition",
+        "location": "Kolkata, India",
+        "flag": "in",
+        "cfp": "31 March 2024",
+        "date": "December 01-05, 2024",
+        "link": "https://icpr2024.org"
+    },{
+        "hindex": 78,
+        "abbrev": "IROS 2024",
+        "title": "IEEE/RSJ International Conference on Intelligent Robots and Systems",
+        "location": "Abu Dhabi, UAE",
+        "flag": "ae",
+        "cfp": "1 March 2024 (TBA)",
+        "date": "October 14-18, 2024",
+        "link": "http://iros2024-abudhabi.org"
+    },{
+        "hindex": 238,
+        "abbrev": "ECCV 2024",
+        "title": "European Conference on Computer Vision",
+        "location": "Milano, Italy",
+        "flag": "it",
+        "cfp": "29 February 2024",
+        "date": "September 29-04, 2024",
+        "link": "https://eccv2024.ecva.net"
+    },{
+        "hindex": 61,
+        "abbrev": "ICIP 2024",
+        "title": "IEEE International Conference on Image Processing",
+        "location": "Abu Dhabi, UAE",
+        "flag": "ae",
+        "cfp": "24 February 2024",
+        "date": "October 27-30, 2024",
+        "link": "https://2024.ieeeicip.org"
+    },{
+        "hindex": 61,
+        "abbrev": "RSS 2024",
+        "title": "Robotics: Science and Systems",
+        "location": "Delft, Netherlands",
+        "flag": "nl",
+        "cfp": "3 February 2024 (TBA)",
+        "date": "July 15-19, 2024",
+        "link": "https://roboticsconference.org"
+    },{
+        "hindex": 254,
+        "abbrev": "ICML 2024",
+        "title": "International Conference on Machine Learning",
+        "location": "Vienna, Austria",
+        "flag": "at",
+        "cfp": "26 January 2024 (TBA)",
+        "date": "July 21-27, 2024",
+        "link": "https://icml.cc/Conferences/2024"
+    },{
+        "hindex": 133,
+        "abbrev": "IJCAI 2024",
+        "title": "International Joint Conference on Artificial Intelligence",
+        "location": "Jeju Island, South Korea",
+        "flag": "kr",
+        "cfp": "11 January 2024 (TBA)",
+        "date": "August 03-09, 2024",
+        "link": "https://www.ijcai.org"
+    },{
+        "hindex": 422,
+        "abbrev": "CVPR 2024",
+        "title": "IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "location": "Seattle, USA",
+        "flag": "us",
+        "cfp": "10 November 2023",
+        "date": "June 17-21, 2024",
+        "link": "https://cvpr.thecvf.com"
+    },{
+        "hindex": "/",
+        "abbrev": "Oceans 2024",
+        "title": "IEEE OCEANS 2024 Singapore",
+        "location": "Singapore, Singapore",
+        "flag": "sg",
+        "cfp": "31 October 2023",
+        "date": "April 14-18, 2024",
+        "link": "https://singapore24.oceansconference.org"
+    },{
+        "hindex": 303,
+        "abbrev": "ICLR 2024",
+        "title": "International Conference on Learning Representations",
+        "location": "Vienna, Austria",
+        "flag": "at",
+        "cfp": "23 September 2023",
+        "date": "May 07-10, 2024",
+        "link": "https://iclr.cc/Conferences/2024"
+    },{
+        "hindex": 119,
+        "abbrev": "ICRA 2024",
+        "title": "IEEE Conference on Robotics and Automation",
+        "location": "Yokohama, Japan",
         "flag": "jp",
-        "cfp": "03 January 2024",
-        "date": "July 18-21, 2024",
-        "link": "https://github.com/"
+        "cfp": "15 September 2023",
+        "date": "May 13-17, 2024",
+        "link": "https://2024.ieee-icra.org"
     }
 ]
--- src/script.js
+++ src/script.js
@@ -4,22 +4,37 @@ function generateTable() {
         let row = table.insertRow();
         let cell = row.insertCell();
         cell.innerText = conf["hindex"];
+        cell.style = "width:8%;text-align:center;"
         cell = row.insertCell();
-        cell.innerText = conf["title"] + ", " + conf["abbrev"];
+        cell.innerHTML = `<strong>${conf["abbrev"]}</strong><strong>:</strong>&nbsp;${conf["title"]}`;
+        cell.style = "width:20%;"
         cell = row.insertCell();
-        cell.innerText = conf["location"];
+        cell.innerHTML = `${conf["location"]} <img style="vertical-align:middle;" src="../pics/${conf["flag"]}.png">`
+        cell.style = "width:20%;"
         cell = row.insertCell();
         cell.innerText = conf["cfp"];
+        cell.style = "width:16%;"
         cell = row.insertCell();
         cell.innerText = conf["date"];
+        cell.style = "width:16%;"
         cell = row.insertCell();
-        cell.innerText = conf["link"];
+        cell.innerHTML = `<a href="${conf["link"]}">${conf["link"]}</a>`;
+        cell.style = "width:20%;"
         // for (key in conf) {
         //     let cell = row.insertCell();
         //     let text = document.createTextNode(conf[key]);
         //     cell.appendChild(text);
         // }
     }
+}
+
+function get_on_click(){
+    var table=document.getElementsByTagName("table")[0];
+    var t_hand=document.getElementsByTagName('tr')[0];
+    var tds = t_hand.querySelectorAll('td');
+    tds[3].onclick = makeSortable_by_SubmissionDeadline;
+    tds[4].onclick = makeSortable_by_ConferenceDate;
+    tds[0].onclick = makeSortable_by_H_Index;
 }
 
 window.onload=function(){
