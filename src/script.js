6887ed23637fdb23debbe25a2e25a765ce0f549b
Alexandre Cardaillac <cardaillac.alexandre@gmail.com>
2023-11-29 08:34:59 +1100

    Refactor js

--- src/script.js
+++ src/script.js
@@ -5,81 +5,59 @@ function generateTable(table) {
         let cell = row.insertCell();
         cell.innerText = conf["hindex"];
         cell.scope="row"
-        // cell.style = "width:8%;"
         cell = row.insertCell();
         cell.innerHTML = `<strong>${conf["abbrev"]}</strong><strong>:</strong>&nbsp;${conf["title"]}`;
-        // cell.style = "width:20%;"
         cell = row.insertCell();
         cell.innerHTML = `${conf["location"]} <img style="vertical-align:middle;" src="../pics/${conf["flag"]}.png">`
-        // cell.style = "width:20%;"
         cell = row.insertCell();
         cell.innerText = conf["cfp"];
-        // cell.style = "width:16%;"
         cell = row.insertCell();
         cell.innerText = conf["date"];
-        // cell.style = "width:16%;"
         cell = row.insertCell();
         cell.innerHTML = `<a href="${conf["link"]}">${conf["link"]}</a>`;
-        // cell.style = "width:10%;"
-        // for (key in conf) {
-        //     let cell = row.insertCell();
-        //     let text = document.createTextNode(conf[key]);
-        //     cell.appendChild(text);
-        // }
     }
 }
 
-function get_H_Index(x){
-    var tds = x.querySelectorAll('td')
-    td = tds[0]
-    if (td.innerText == "H-Index"){
-        if (hi_toggle) {
-            return -100000;
-        }
-        return Infinity;
-    }
+function getHIndex(x){
+    let td = x.querySelectorAll('td')[0]
+    let delta = 0;
 
-    var delta = 0;
     try {
         delta = parseInt(td.innerText);
     }
     catch(e){
             return 0;
     }
-
     if (isNaN(delta)){
         return 0;
     }
-
     return delta;
 }
 
-var hi_toggle = false;
-function makeSortable_by_H_Index() {
-    var table=document.getElementsByTagName("table")[0];
-    var flag=false;
-
-    var tbody=table.tBodies[0];
-    var rows=tbody.getElementsByTagName("tr");
-    rows=Array.prototype.slice.call(rows,0);
+let hi_toggle = false;
+function sortHIndex() {
+    let table = document.getElementsByTagName("table")[0];
+    let tbody = table.tBodies[0];
+    let rows = tbody.getElementsByTagName("tr");
+    rows = Array.prototype.slice.call(rows,0);
 
     rows.sort(function(x,y){
-        var tx = get_H_Index(x);
-        var ty = get_H_Index(y);
+        let tx = getHIndex(x);
+        let ty = getHIndex(y);
         if (hi_toggle) {
             return tx - ty;
         }
         return ty - tx;
 
     });
-    for(var i=0;i<rows.length;i++){
+    for(let i = 0; i < rows.length; i++){
         tbody.appendChild(rows[i]);
     }
     hi_toggle = !hi_toggle;
 }
 
-function get_submission_time(x, is_conf){
-    var tds = x.querySelectorAll('td');
+function getTime(x, is_conf){
+    let tds = x.querySelectorAll('td');
     if (is_conf) {
         td = tds[4];
         toggle = sc_toggle
@@ -88,16 +66,8 @@ function get_submission_time(x, is_conf){
         toggle = sd_toggle
     }
 
-    if (td.innerText == "Submission Deadline" ||
-        td.innerText == "Conference Date"){
-        if (toggle) {
-            return Infinity;
-        }
-        return -1;
-    }
-
-    var delta = 36600;
-    var tt = td.innerText;
+    let delta = 36600;
+    let tt = td.innerText;
     items = tt.split("-");
     if (items.length > 1) {
         iitems = items[1].split(",");
@@ -106,7 +76,7 @@ function get_submission_time(x, is_conf){
         }
     }
     try {
-        var targetDate = Date.parse(tt)
+        let targetDate = Date.parse(tt)
         if (targetDate < Date.now()) {
             if (toggle) {
                 delta = -1;
@@ -129,77 +99,55 @@ function get_submission_time(x, is_conf){
     return delta;
 }
 
-var sd_toggle = false;
-function makeSortable_by_SubmissionDeadline() {
-    var table=document.getElementsByTagName("table")[0];
-    var flag = false;
+let sd_toggle = false;
+let sc_toggle = false;
+function sortTime(is_conf) {
+    let table = document.getElementsByTagName("table")[0];
+    let tbody = table.tBodies[0];
+    let rows = tbody.getElementsByTagName("tr");
+    rows = Array.prototype.slice.call(rows, 0);
 
-    var tbody=table.tBodies[0];
-    var rows=tbody.getElementsByTagName("tr");
-    rows=Array.prototype.slice.call(rows,0);
+    if (is_conf) {
+        toggle = sc_toggle
+    } else {
+        toggle = sd_toggle
+    }
 
     rows.sort(function(x,y){
-        var tx = get_submission_time(x, false);
-        var ty = get_submission_time(y, false);
-        if (sd_toggle) {
+        let tx = getTime(x, is_conf);
+        let ty = getTime(y, is_conf);
+        if (toggle) {
             return ty - tx;
         }
         return tx - ty;
 
     });
-    for(var i=0;i<rows.length;i++){
+    for(let i=0;i<rows.length;i++){
         tbody.appendChild(rows[i]);
     }
-    sd_toggle = !sd_toggle
-}
-
-
-
-var sc_toggle = false;
-function makeSortable_by_ConferenceDate() {
-    console.log("makeSortable_by_ConferenceDate")
-    var table=document.getElementsByTagName("table")[0];
-    var flag=false;
-
-    var tbody=table.tBodies[0];
-    console.log(tbody)
-    var rows=tbody.getElementsByTagName("tr");
-    console.log(rows)
-    rows=Array.prototype.slice.call(rows,0);
-
-    rows.sort(function(x,y){
-        var tx = get_submission_time(x, true);
-        var ty = get_submission_time(y, true);
-        if (sc_toggle) {
-            return ty - tx;
-        }
-        return tx - ty;
-    });
-    for(var i=0;i<rows.length;i++){
-        tbody.appendChild(rows[i]);
+    if (is_conf) {
+        sc_toggle = !sc_toggle
+    } else {
+        sd_toggle = !sd_toggle
     }
-    sc_toggle = !sc_toggle
 }
 
-function get_on_click(table){
-    // var table=document.getElementsByTagName("table")[0];
-    var t_hand=document.getElementsByTagName('tr')[0];
-    var tds = t_hand.querySelectorAll('th');
-    tds[3].onclick = makeSortable_by_SubmissionDeadline;
-    tds[4].onclick = makeSortable_by_ConferenceDate;
-    tds[0].onclick = makeSortable_by_H_Index;
+function onClick(table){
+    let tds = table.tHead.querySelectorAll('th');
+    tds[3].onclick = () => sortTime(false);
+    tds[4].onclick = () => sortTime(true);
+    tds[0].onclick = sortHIndex;
 }
 
 window.onload=function(){
-    console.log(conferences);
     let table = document.querySelector("table");
     generateTable(table);
-    get_on_click(table);
+    onClick(table);
     document.querySelectorAll('tr').forEach(function(item) {
-        var tds = item.querySelectorAll('td'),
+        let tds = item.querySelectorAll('td'),
             td = tds[3]
         try {
-            var targetDate = Date.parse(td.innerText)
+            let targetDate = Date.parse(td.innerText)
             if (targetDate < Date.now()) {
                 td.style.color = 'gray'
                 td.style.textDecoration = 'line-through'
@@ -207,12 +155,12 @@ window.onload=function(){
             else if (targetDate - Date.now() < 1000*60*60*24*120) {
                 td.style.color = 'red'
                 td.style.fontWeight = 'bolder'
-                var delta = targetDate - Date.now()
-                var days = Math.floor(delta/(24*60*60*1000))
+                let delta = targetDate - Date.now()
+                let days = Math.floor(delta/(24*60*60*1000))
                 td.innerText = td.innerText + ' (' + days.toString() + ')'
             }
         }
         catch(e){}
     });
-    makeSortable_by_SubmissionDeadline();
+    sortTime(false);
 }
